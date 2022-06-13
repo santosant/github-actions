a545855baeeb5fc05dd34d92f250108560012dea
Antonio Anderson <hiroto.anderson@gmail.com>
2022-06-13 13:40:31 -0300

    fix: tests

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -4,7 +4,7 @@ describe('Math', () => {
   it('should be able to sum A and B', () => {
     expect(sum(2, 2)).toBe(4);
   });
-  it('should be fail', () => {
-    expect(sum(2, 2)).toBe(5);
+  it('should be able to get result 5', () => {
+    expect(sum(2, 3)).toBe(5);
   });
 });
